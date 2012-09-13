ba37eca0e1c8f11b28520e5fc1f833f7b5237f45
Tom <thomas@clicker360.com>
2012-09-13 17:33:37 -0500

    Agregar funciones de timestamp>sec y sec>timestamp

--- functions.php
+++ functions.php
@@ -19,10 +19,58 @@ function isavailable() {
 	}
 }
 
+function tstosec($timestamp) {
+	$return = 0;
+	$h = 0;
+	$i = 0;
+	$s = 0;
+	if(strlen($timestamp) == 14) {
+		$y = substr($timestamp, 0, 4);
+		$m = substr($timestamp, 4, 2);
+		$d = substr($timestamp, 6, 2);
+		$h = substr($timestamp, 8, 1) == 0? substr($timestamp, 9, 1) : substr($timestamp, 8, 2);
+		$i = substr($timestamp, 10, 1) == 0? substr($timestamp, 11, 1) : substr($timestamp, 10, 2);
+		$s = substr($timestamp, 12, 1) == 0? substr($timestamp, 13, 1) : substr($timestamp, 12, 2);
+	} elseif(strlen($timestamp) == 6) {
+		$h = substr($timestamp, 0, 1) == 0? substr($timestamp, 1, 1) : substr($timestamp, 0, 2);
+		$i = substr($timestamp, 2, 1) == 0? substr($timestamp, 3, 1) : substr($timestamp, 2, 2);
+		$s = substr($timestamp, 4, 1) == 0? substr($timestamp, 5, 1) : substr($timestamp, 4, 2);
+	}
+	$h = (int) $h;
+	$i = (int) $i;
+	$s = (int) $s;
+	$hs = $h*3600;
+	$is = $i*60;
+	$return = $hs+$is+$s;
+	return $return;
+}
+
+function sectots($sec){
+	$h = intval($sec / 3600);
+	$sec = $sec % 3600;
+	$m = intval($sec / 60);
+	$sec = $sec % 60;
+	if($h < 10) {
+		$h = 0 . $h;
+	}
+	if($m < 10) {
+		$m = 0 . $m;
+	}
+	if($sec < 10) {
+		$sec = 0 . $sec;
+	}
+	return $h . $m . $sec;
+}
+/*
 if(isavailable()){
 	echo "1";	
 } else {
 	echo "0";
 }
+*/
 
+$ts1 = tstosec('20120911182400');
+$ts2 = tstosec('182400');
+$ts3 = sectots($ts2);
+echo "-> " . $ts1 . " -> " . $ts2 . " -> " . $ts3;
 ?>
